1f7dbbb32e6f5ffb1ae70020e3466ded0c02f04f
wwJavid <javababayev@gmail.com>
2023-02-18 12:26:23 +0200

    fix head stylesheet issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/zjq5uxj.css"
+        ></link>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
