a1776bfdb85659938924d5916329c0206f9bb4c1
Sakina <sakinaibrahimova7@gmail.com>
2023-02-22 18:28:01 +0200

    scrollOne

--- modules/scroll-one/ScrollOne.styles.ts
+++ modules/scroll-one/ScrollOne.styles.ts
@@ -4,15 +4,16 @@ export const ScrollOneStyles = (theme: Theme) => ({
   fontWeight: 700,
   color: theme.palette.primary.main,
   fontFamily: theme.typography.fontFamily,
+  fontSize: "26px",
 });
 
 export const ScrollOneContainerStyles = (theme: Theme) => ({
   display: "flex",
   flexDirection: "column",
-  // alignItems: "start",
   justifyContent: "center",
   justifyItems: "center",
   height: "884px",
   width: "100%",
-  backgroundColor: theme.palette.background.default,
+  backgroundColor: "#C7F5FF",
+  padding: "2rem",
 });
--- modules/scroll-one/ScrollOne.tsx
+++ modules/scroll-one/ScrollOne.tsx
@@ -6,11 +6,21 @@ const ScrollOne = () => {
   const headingText = {
     brand: "paradoks, ",
     slogan: "dünyaya fərqli perspektivdən baxan bir platforma",
+    secondSlogan:
+      "Fərqli ölkələrdən maraqlı  mövzular əsasında araşdırmalar və video materiallar",
   };
 
   return (
     <Box sx={(theme) => ScrollOneContainerStyles(theme)}>
-      <Box sx={{ width: "fit-content", backgroundColor: "yellow" }}>
+      <Box
+        sx={{
+          position: "absolute",
+          width: "725px",
+          height: "82px",
+          left: "357.5px",
+          top: "256px",
+        }}
+      >
         <Typography
           variant="h6"
           component={"span"}
@@ -20,13 +30,49 @@ const ScrollOne = () => {
           <Typography variant="h6">{headingText.slogan}</Typography>
         </Typography>
       </Box>
-      <Box>
-        <Image
-          src="/images/imageScrollOne.png"
-          alt="Image Scroll One"
-          width={933}
-          height={705}
-        />
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "flex-end",
+
+          border: "1px solid white",
+
+          width: "651.5px",
+          height: "159.5px",
+          left: "-0.5px",
+          top: "200px",
+          backgroundColor: "white",
+          color: "white",
+        }}
+      >
+        <Box>
+          <Image
+            src="/images/imageScrollOne.png"
+            alt="Image Scroll One"
+            width={915}
+            height={513}
+          />
+        </Box>
+        <Typography
+          sx={{
+            width: "473px",
+            height: "87px",
+            left: "calc(50% - 473px/2 - 3.5px)",
+            top: "calc(50% - 87px/2)",
+            fontFamily: "Inter",
+            fontStyle: "normal",
+            fontWeight: "400",
+            fontSize: "19px",
+            lineHeight: "29px",
+            bgColor: "white",
+            color: "black",
+          }}
+        >
+          {headingText.secondSlogan}
+        </Typography>
       </Box>
     </Box>
   );
