ec23e3c95d122ad0993f90cafee4c64855dfb29c
wwJavid <javababayev@gmail.com>
2023-03-04 23:09:12 +0200

    open menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,9 @@ import {
 } from "@mui/material";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Home from "./styles/Home.modules";
 import { createContext, useMemo, useState } from "react";
 
-export const ColorModeContext = createContext({ toggleColorMode: () => {} });
+export const ColorModeContext = createContext({ toggleColorMode: () => { } });
 
 export default function App({ Component, pageProps }: AppProps) {
   const [mode, setMode] = useState<PaletteMode>("light");
